d1dedd249e121827263c97937b22d1c4be66e126
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-09-10 00:01:18 +0700

    feat: creat router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import CartPage from "./pages/CartPage/CartPage";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
+import ErrorNotFound from "./pages/ErrorNotFound/ErrorNotFound";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import ShopPage from "./pages/ShopPage/ShopPage";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+      errorElement: <ErrorNotFound />
+    },
+    {
+      path: "/cart",
+      element: <CartPage />,
+      errorElement: <ErrorNotFound />
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+      errorElement: <ErrorNotFound />
+    },
+    {
+      path: "/product",
+      element: <ProductPage />,
+      errorElement: <ErrorNotFound />
+    },
+    {
+      path: "/shop",
+      element: <ShopPage />,
+      errorElement: <ErrorNotFound />
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+      errorElement: <ErrorNotFound />
+    },
+    {
+      path: "/checkout",
+      element: <CheckoutPage />,
+      errorElement: <ErrorNotFound />
+    }
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
