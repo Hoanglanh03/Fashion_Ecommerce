050b7a2f8c6a17f1e745a281a4175a6fc019ddde
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-09-22 16:25:33 +0700

    feat: Build login form

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -6,7 +6,6 @@ import img_1 from "../../asserts/images/ads.png";
 import axios from "axios";
 
 const LoginPage = () => {
-  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
@@ -23,34 +22,25 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const newErrors = validateForm();
-    setErrors(newErrors);
+    try {
+      const loginData = {
+        email: formData.email,
+        password: formData.password,
+      };
 
-    if (Object.keys(newErrors).length === 0) {
-      console.log(Object.keys(newErrors));
-      try {
-        const loginData = {
-          email: formData.email,
-          password: formData.password,
-        };
+      const response = await axios.post(
+        "https://fakestoreapi.com/users",
+        loginData
+      );
 
-        const response = await axios.post(
-          "https://fakestoreapi.com/users",
-          loginData
-        );
-
-        if (response.status === 200) {
-          toast.success("Login successfully");
-          navigate("/");
-        } else {
-          toast.error("Login Unsuccessful!");
-        }
-      } catch (error) {
-        console.error("There was an error making the request!", error);
-        toast.error("Failed to login, please try again.");
+      if (response.status === 200) {
+        toast.success("Login successfully");
+        navigate("/");
+      } else {
+        toast.error("Login Unsuccessful!");
       }
-    } else {
-      toast.error("Please fill out all required fields.");
+    } catch (error) {
+      toast.error("Failed to login, please try again.");
     }
   };
 
@@ -72,8 +62,6 @@ const LoginPage = () => {
             onChange={handleChange}
             required
           />
-          {errors.email && <p className="error-message">{errors.email}</p>}
-
           <input
             type="password"
             name="password"
@@ -83,11 +71,8 @@ const LoginPage = () => {
             minLength="2"
             required
           />
-          {errors.password && (
-            <p className="error-message">{errors.password}</p>
-          )}
-          <a href="/terms">Forget Password?</a>
 
+          <a href="/terms">Forget Password?</a>
           <button type="submit">Sign In</button>
         </form>
 
