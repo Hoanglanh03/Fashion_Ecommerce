aaafd9bb2a2a7d085813fb47241636e92df59686
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-10-16 16:55:24 +0700

    feat: add useEffect to set localStorage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import ShopPage from "./pages/ShopPage/ShopPage";
-
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadUserFromLocalStorage } from "./redux/slices/authSlice";
 function App() {
   const router = createBrowserRouter([
     {
@@ -48,6 +50,12 @@ function App() {
     },
   ]);
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    console.log("change");
+    dispatch(loadUserFromLocalStorage());
+  }, [dispatch]);
   return (
     <div>
       <Toaster />
