4c7ef6942126e95a25a2e72b6833a86bc807cf63
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-10-21 19:05:19 +0700

    feat: build shoppage

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,17 +1,79 @@
-import React from "react";
-import Header from "../../layout/header";
-import Footer from "../../layout/footer";
-import BannerShop from "./BannerShop";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Header from "../../components/header";
 import Sidebar from "./Sidebar";
 import ProductGird from "./ProductGird";
-import Productfilter from "./Productfilter";
+import Footer from "../../components/footer";
+import Banner from "../../asserts/images/banner.avif";
 
 const ShopPage = () => {
+  const [selectedCard, setSelectedCard] = useState("null");
+  const [categories, setCategories] = useState([]);
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedPriceRange, setSelectedPriceRange] = useState([0, 1000]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        if (response.status === 200) {
+          setData(response.data);
+
+          const allCategories = [
+            ...new Set(response.data.map((ele) => ele.category)),
+          ];
+
+          setCategories(allCategories);
+          setSelectedCard(allCategories[0]);
+        } else {
+          toast.error("Not contact with API");
+        }
+      } catch (error) {
+        console.error("Error occurred while fetching data:", error);
+        alert("Error:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Header />
-      <BannerShop />
-      <Productfilter />
+      <div className=" relative h-auto md:h-96 md:w-full">
+        <img
+          className="w-full md:h-full md:w-full object-fit"
+          src={Banner}
+          alt=""
+        />
+        <div className=" absolute flex flex-col  items-center justify-between inset-y-2/4 ml-11  gap-2">
+          <h1 className=" text-[black] cursor-pointer text-2xl md:text-4xl leading-none font-medium ">
+            WOMEN'S FASHION
+          </h1>
+          <p className="text-[black] cursor-pointer text-xl leading-none font-light mx-auto  ">
+            SAVE UP TO 40% OFF
+          </p>
+        </div>
+      </div>
+
+      <div className="container md:w-full lg:w-full flex justify-between ">
+        <Sidebar
+          categories={categories}
+          selectedCard={selectedCard}
+          setSelectedCard={setSelectedCard}
+          setSearchTerm={setSearchTerm}
+          setSelectedPriceRange={setSelectedPriceRange}
+        />
+
+        <ProductGird
+          data={data}
+          selectedCard={selectedCard}
+          searchTerm={searchTerm}
+          selectedPriceRange={selectedPriceRange}
+        />
+      </div>
+
       <Footer />
     </div>
   );
