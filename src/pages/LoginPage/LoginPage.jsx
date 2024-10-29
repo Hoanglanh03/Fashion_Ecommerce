74d7e02797ecdc4dfe39a39e0ec72b71dcb0f2ba
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-10-29 08:52:26 +0700

    feat: update login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "../../redux/state";
 import toast from "react-hot-toast";
 import img_1 from "../../asserts/images/ads.png";
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { setLogin } from "../../redux/state";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
