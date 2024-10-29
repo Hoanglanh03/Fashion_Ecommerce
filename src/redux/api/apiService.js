e1374a7d71c8553dde5dac0f0b7d025dd0f4f224
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-10-29 08:50:16 +0700

    feat: create apiService to call API

---
+++ src/redux/api/apiService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+import { setProducts } from "../state";
+
+export const fetchProductById = async (id) => {
+  try {
+    const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.log("Not contact with API");
+    }
+  } catch (error) {
+    console.error("Error fetching product data:", error);
+  }
+};
+
+export const fetchProductALL = () => async (dispatch) => {
+  try {
+    const response = await axios.get(`https://fakestoreapi.com/products/`);
+    if (response.status === 200) {
+      dispatch(setProducts({ products: response.data }));
+    } else {
+      console.log("Could not fetch products from API");
+    }
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+};
