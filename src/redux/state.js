9099bb484e1c40fcd4fce51a977bc7bc6284c3f3
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-11-06 08:17:39 +0700

    feat: update state file

--- src/redux/state.js
+++ src/redux/state.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   email: null,
   products: [],
+  cart: [],
 };
 
 export const userSlice = createSlice({
@@ -18,9 +19,44 @@ export const userSlice = createSlice({
     setProducts: (state, action) => {
       state.products = action.payload.products;
     },
+    setCart: (state, action) => {
+      state.cart = action.payload.cart;
+    },
+    addToCart: (state, action) => {
+      const product = action.payload;
+      const existingProduct = state.cart.find((item) => item.id === product.id);
+
+      if (existingProduct) {
+        existingProduct.quantity += product.quantity;
+      } else {
+        state.cart.push({ ...product, quantity: product.quantity });
+      }
+    },
+    increaseQuantity: (state, action) => {
+      const product = state.cart.find((item) => item.id === action.payload);
+      if (product) {
+        product.quantity += 1;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const product = state.cart.find((item) => item.id === action.payload);
+      if (product && product.quantity > 1) {
+        product.quantity -= 1;
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((item) => item.id !== action.payload);
+    },
   },
 });
 
-export const { setLogin, setLogout, setProducts } =
-  userSlice.actions;
+export const {
+  setLogin,
+  setLogout,
+  setProducts,
+  addToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+} = userSlice.actions;
 export default userSlice.reducer;
